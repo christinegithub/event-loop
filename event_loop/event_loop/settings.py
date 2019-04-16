106d9bb539a188944655cf889f19577ea8ff0782
Anton <anton.mois33v@gmail.com>
2019-04-16 16:50:33 -0400

    Added a simple map to the home page

--- event_loop/event_loop/settings.py
+++ event_loop/event_loop/settings.py
@@ -12,11 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-GOOGLE_MAPS_KEY = os.environ.get("GOOGLE_MAPS_KEY")
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
