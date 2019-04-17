66937a3104cfe6303c282bf799e8f5bd90ac8895
Myles Bennett <myles.bennett@hotmail.com>
2019-04-17 11:26:41 -0400

    added profile

--- event_loop/event_loop/urls.py
+++ event_loop/event_loop/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('login/', views.login_view, name="login"),
     path('signup/', views.signup, name="signup"),
     path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile, name='profile'),
+    path('profiles/create', views.profile_create, name="profile_create"),
 ]
--- event_loop/event_loop/views.py
+++ event_loop/event_loop/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.conf import settings
-from event_loop.forms import LoginForm
+from event_loop.forms import LoginForm, ProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 
 import json
 import requests
@@ -21,13 +23,13 @@ def home_page(request):
     event_body = json.loads(event_response.content)
 
     for event in event_body["results"]:
-        # each_event = requests.get(f"https://www.blogto.com/api/v2/events/{event['id']}")
-        # each_event_body = json.loads(each_event.content)
-        # # print(event["title"])
-        # if each_event_body["location"]:
-        #     event_location = Location.objects.create(address = each_event_body["address"], province = each_event_body["province"], city = each_event_body["city"], longitude = each_event_body["location"]["longitude"], latitude = each_event_body["location"]["latitude"])
-        # else:
-        #     event_location = None
+        each_event = requests.get(f"https://www.blogto.com/api/v2/events/{event['id']}")
+        each_event_body = json.loads(each_event.content)
+        # print(event["title"])
+        if each_event_body["location"]:
+            event_location = Location.objects.create(address = each_event_body["address"], province = each_event_body["province"], city = each_event_body["city"], longitude = each_event_body["location"]["longitude"], latitude = each_event_body["location"]["latitude"])
+        else:
+            event_location = None
         new_event = Event.objects.create(title = event["title"], description = event["description_stripped"], date = date, start_time = event["start_time"], end_time = event["end_time"], blogto_id = event["id"])
 
 
@@ -47,12 +49,36 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return HttpResponseRedirect('/home/')
+            return redirect(reverse('profile'))
     else:
         form = UserCreationForm()
 
     response = render(request, 'signup.html', {'form': form})
     return HttpResponse(response)
+
+@login_required
+def profile(request):
+    context = {'title': 'Profile'}
+    if not Profile.exists_for_user(request.user):
+        form = ProfileForm()
+        context['form'] = form
+    response = render(request, 'profile.html', context)
+    return HttpResponse(response)
+
+
+@login_required
+def profile_create(request):
+    form = ProfileForm(request.POST)
+    form.instance.user = request.user
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/profile/')
+    else:
+        context = {'title': 'Profile', 'form': form}
+        response = render(request, 'profile.html', context)
+        return HttpResponse(response)
+
+    
 
 def login_view(request):
     if request.user.is_authenticated:
