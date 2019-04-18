38404d7d1de865dade3bd0623aacd3c0c4d776bb
Anton <anton.mois33v@gmail.com>
2019-04-18 17:12:33 -0400

    set up basic react routing

--- event_loop/event_loop_frontend/src/App.js
+++ event_loop/event_loop_frontend/src/App.js
@@ -1,36 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  state = {
-    events: []
-  };
-
-  async componentDidMount() {
-    try {
-      const res = await fetch('http://127.0.0.1:8000/api/');
-      const events = await res.json();
-      this.setState({
-        events
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  render() {
-    return (
+function App() {
+  return (
+    <Router>
       <div>
-        {this.state.events.map(item => (
-          <div key={item.id}>
-            <h1>{item.title}</h1>
-            <span>{item.description}</span>
-          </div>
-        ))}
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/events/:id" component={Event} />
       </div>
-    );
-  }
+    </Router>
+  );
+}
+
+function Event({ match }) {
+  return (
+    <div>
+      <h3>Event: {match.params.id}</h3>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Home({ match }) {
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`events/1`}>Event #1</Link>
+        </li>
+      </ul>
+
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a topic.</h3>}
+      />
+    </div>
+  );
+}
+
+function Topic({ match }) {
+  return (
+    <div>
+      <h3>{match.params.topicId}</h3>
+    </div>
+  );
 }
 
 export default App;
